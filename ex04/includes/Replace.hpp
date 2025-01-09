5eb736cc556e1f2699e96694ec6535c1ebe3d134
Charlie Mathot <charliemathot@MacBook-Air-de-Charlie.local>
2025-01-09 10:14:52 +0100

    ex04 begin

---
+++ ex04/includes/Replace.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmathot <cmathot@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 22:59:23 by cmathot           #+#    #+#             */
+/*   Updated: 2025/01/08 23:12:00 by cmathot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REPLACE_HPP
+# define REPLACE_HPP
+# include <iostream>
+# include <fstream>
+
+class Replace
+{
+public:
+
+private:
+
+};
+
+#endif
